528abf653688102896e7b239b1465f87bfe1ab1e
BIueLightning <johannes-franzen@t-online.de>
2021-06-23 15:54:12 +0200

    extracted glsl strings into own .ts file

--- src/GreenScreenStream.ts
+++ src/GreenScreenStream.ts
@@ -18,6 +18,7 @@ import {
     VideoTextureSettings
 } from './texturesettings';
 import { asyncCall } from './async-call.util';
+import { BUFFER_FRAG, BUFFER_VERT, MAIN_FRAG, MAIN_VERT } from './glsl-constants';
 
 export enum GreenScreenMethod {
     Mask = 0,
@@ -45,69 +46,13 @@ export class GreenScreenStream {
     private maskRange = { x: 0.0025, y: 0.26 }
     private useML: boolean;
 
-    mainFrag: string = `uniform vec2 resolution;
-    uniform sampler2D A;
-    out vec4 fragColor;
-    void main(){
-        vec2 uv = gl_FragCoord.xy/resolution.xy;
-        fragColor = texture(A, uv);
-    }`;
+    mainFrag: string = MAIN_FRAG;
 
-    mainVert: string = `layout(location = 0) in vec2 pos;
-    out vec4 fragColor;
-    void main() {
-        gl_Position = vec4(pos.xy,0.0,1.0);
-    }
-    `;
-    bufferVert: string = `layout(location = 0) in vec2 pos;
-    out vec4 fragColor;
-    void main() {
-        gl_Position = vec4(pos.xy,0.0,1.0);
-    }
-    `;
+    mainVert: string = MAIN_VERT;
 
-    bufferFrag: string = `uniform float time;
-    uniform vec2 resolution;
-    uniform sampler2D webcam;
-    uniform sampler2D background;
-    uniform vec4 chromaKey;
-    uniform vec2 maskRange;
-    out vec4 fragColor;
+    bufferVert: string = BUFFER_VERT;
 
-    mat4 RGBtoYUV = mat4(0.257,  0.439, -0.148, 0.0,
-        0.504, -0.368, -0.291, 0.0,
-        0.098, -0.071,  0.439, 0.0,
-        0.0625, 0.500,  0.500, 1.0 );
-
-float colorclose(vec3 yuv, vec3 keyYuv, vec2 tol)
-{
-float tmp = sqrt(pow(keyYuv.g - yuv.g, 2.0) + pow(keyYuv.b - yuv.b, 2.0));
-if (tmp < tol.x)
-return 0.0;
-else if (tmp < tol.y)
-return (tmp - tol.x)/(tol.y - tol.x);
-else
-return 1.0;
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-vec2 fragPos =  1. - fragCoord.xy / resolution.xy;
-vec4 fg = texture(webcam, fragPos);
-vec4 bg = texture(background, fragPos);
-
-vec4 keyYUV =  RGBtoYUV * chromaKey;
-vec4 yuv = RGBtoYUV * fg;
-
-float mask = 1.0 - colorclose(yuv.rgb, keyYUV.rgb, maskRange);
-
-fragColor = max(fg - mask * chromaKey, 0.0) + bg * mask;
-
-}
-
-void main(){
-    mainImage(fragColor,gl_FragCoord.xy);
-}`;
+    bufferFrag: string = BUFFER_FRAG;
 
     constructor(public greenScreenMethod: GreenScreenMethod, public canvas?: HTMLCanvasElement, width: number = 640, height: number = 360) {
         this.mediaStream = new MediaStream();
