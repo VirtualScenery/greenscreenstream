075bd27d780fe00894be1125be31152bd2e32e23
BIueLightning <johannes-franzen@t-online.de>
2022-02-13 21:23:10 +0100

    Fixed wrong SD Resolution value

--- src/utils/resolution-from-enum.util.ts
+++ src/utils/resolution-from-enum.util.ts
@@ -8,7 +8,7 @@ export function resolutionFromEnum(videoResolution: VideoResolution): Vector2 {
         case (VideoResolution.UHD):
             resolution = new Vector2(3840, 2160);
             break;
-            case (VideoResolution.WQHD):
+        case (VideoResolution.WQHD):
             resolution = new Vector2(2560, 1440);
             break;
         case (VideoResolution.FullHD):
@@ -18,9 +18,8 @@ export function resolutionFromEnum(videoResolution: VideoResolution): Vector2 {
             resolution = new Vector2(1280, 720);
             break;
         case (VideoResolution.SD):
-            resolution = new Vector2(1024, 576);
+            resolution = new Vector2(640, 360);
             break;
     }
-    console.log(resolution.toString(), resolution.toMediaConstraint())
     return resolution;
 }
