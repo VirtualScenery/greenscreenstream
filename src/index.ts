d3f04f7b2116ee7b55c84cdd787fe2cff1feab7a
BIueLightning <johannes-franzen@t-online.de>
2022-02-13 21:27:40 +0100

    Added Vector2 & VideResolution to module exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 
+//Classes
 export { GreenScreenStream } from './green-screen-stream';
+export  { Vector2 } from './models/vector2';
 
+//Enums
 export { GreenScreenMethod } from './models/enums/green-screen-method.enum';
 export { BodyPixMode } from "./models/enums/bodypixmode.enum";
+export { VideoResolution } from './models/enums/video-resolution.enum';
 
+//Interfaces
 export type { IGreenScreenConfig } from './models/green-screen-config.interface';
 export type { IMaskSettings, RGBA } from './models/masksettings.interface';
 export type { IBodyPixConfig} from './models/bodypix-config.interface';
